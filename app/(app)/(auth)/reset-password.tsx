c13a16ad5b7766f8c596584b0d9aa4bf21fc8790
Ahmed Kamal <38223175+tkahmedkamal@users.noreply.github.com>
2026-03-19 03:18:59 +0200

    Add reset password functionality
    - Introduced `ResetPassword` screen and form for resetting user passwords.
    - Updated `AuthLayout` to include the new reset password screen in the navigation stack.
    - Created styles for the reset password screen and form components.
    - Added validation for the reset password form using `zod` and integrated with `react-hook-form`.
    - Updated styles in `screen-header` for improved consistency.

---
+++ app/(app)/(auth)/reset-password.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ResetPasswordScreen from '@/screens/auth/reset-password';
+
+const ResetPassword = () => {
+	return <ResetPasswordScreen />;
+};
+
+export default ResetPassword;
